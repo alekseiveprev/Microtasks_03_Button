9df23d9d6ec1ac9531d3518d0c685fdd916b1efb
alekseiveprev <72622572+alekseiveprev@users.noreply.github.com>
2023-09-18 16:28:52 +0300

    init: start button-task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import { FC } from 'react';
-
+import { FC, MouseEvent } from 'react';
+/* import {MouseEvent} from 'react'; */
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
+  /*   const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    console.log('Hello Im Vasia!');
+  };
+  const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    console.log('Hello Im Ivan!');
+  }; */
+
+  const onClickHandler = (name: string) => {
+    console.log(name);
+  };
+
   return (
-    <div>
-      <{/* h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p> */}
+    <div className="App">
+      {/* <button
+        onClick={(event) => {
+          console.log('Hello');
+        }}
+      >  MyYouTubeChanel-1{' '}
+      </button> */}
+      {/*  <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
+      <button onClick={mySecondSubscriber}>MyYouTubeChanel-1</button> */}
+      <button
+        onClick={(event: MouseEvent<HTMLButtonElement>) =>
+          onClickHandler('Vasia!')
+        }
+      >
+        MyYouTubeChanel-1
+      </button>
+      <button onClick={() => onClickHandler('Ivan')}>MyYouTubeChanel-2</button>
+      <button onClick={() => onClickHandler('some info')}>
+        MyYouTubeChanel-3
+      </button>
     </div>
   );
 };
